0a2bf0248b05d03fc834b127a1bd0403d42623a1
Unknown <chinnareddyjava87@gmail.com>
2018-04-28 23:55:30 -0500

    undo

---
+++ CLI.go
@@ -0,0 +1,17 @@
+package main
+
+//import (
+//	"bufio"
+//	"fmt"
+//	"os"
+//)
+
+//func main() {
+
+//	reader := bufio.NewReader(os.Stdin)
+//	fmt.Println("enter the text")
+
+//	text, _ := reader.ReadString('\n')
+//	fmt.Println(text)
+
+//}
